b333ad9b2b29bbc4c7653df9f2c09fdccaa595aa
区枫华 <906372411@qq.com>
2017-03-19 22:57:38 +0800

    添加各注释

--- app/src/main/java/com/example/ondrawtest/view/LoadingView.java
+++ app/src/main/java/com/example/ondrawtest/view/LoadingView.java
@@ -10,6 +10,8 @@ import android.view.View;
 import java.util.Random;
 
 /**
+ * 彩虹色的LoadingView
+ * <p>
  * Created by 区枫华 on 2017/3/18.
  */
 
@@ -23,24 +25,57 @@ public class LoadingView extends View {
      * 加速度
      */
     private final double ACELERATION = 0.3;
+    /**
+     * 初始间隔
+     */
     private double OFFSET = 0.8;
+    /**
+     * 是否是第一次开始的
+     */
     boolean start = true;
     int radius;
-    static int pointRadius = 50;
 
+    /**
+     * 画笔组
+     */
     private static Paint paintGroup[] = {new Paint(), new Paint(), new Paint(), new Paint(), new Paint(), new Paint()};
+    /**
+     * 画笔颜色组
+     */
     private static int paintARGB[][] = {{255, 255, 168, 1}, {255, 255, 244, 1}, {255, 203, 255, 1}, {255, 89, 222, 157}, {255, 97, 157, 255}, {255, 209, 128, 254}};
+    /**
+     * 角度组
+     */
     private double angleGroup[] = {0, 0, 0, 0, 0, 0};
+    /**
+     * 是否回到原点标记组
+     */
     private boolean zeroGroup[] = {true, true, true, true, true, true};
+    /**
+     * 画笔对应坐标xy组
+     */
     private int xyGroup[][] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}};
+    /**
+     * 各画笔原点前后两次位置角度差距组
+     */
     private double offsetGroup[] = {OFFSET, OFFSET, OFFSET, OFFSET, OFFSET, OFFSET};
+    /**
+     * 运动轨迹的圆心x
+     */
     int x;
+    /**
+     * 运动轨迹的圆心y
+     */
     int y;
 
+    //在类加载的时候进行画笔初始化
     static {
         for (int i = 0; i < paintGroup.length; i++) {
+            //设置画笔颜色
             paintGroup[i].setARGB(paintARGB[i][0], paintARGB[i][1], paintARGB[i][2], paintARGB[i][3]);
-            paintGroup[i].setStrokeWidth(pointRadius);
+            //抗锯齿化
+            paintGroup[i].setAntiAlias(true);
+            //设置画笔形状
             paintGroup[i].setStrokeCap(Paint.Cap.ROUND);
         }
     }
@@ -60,28 +95,35 @@ public class LoadingView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        //计算中心位置
         x = getWidth() / 2;
         y = getHeight() / 2;
+
+        //取最小的一个半径作为运动轨迹的半径
         if (x >= y)
             radius = y / 2;
         else
             radius = x / 2;
 
+        //第一次运行时的初始化
         if (start) {
             for (int i = 0; i < xyGroup.length; i++) {
                 xyGroup[i][0] = x - radius;
                 xyGroup[i][1] = y;
+                paintGroup[i].setStrokeWidth(radius / 3);
             }
             start = false;
         }
 
+        //画出相应的点
         for (int i = paintGroup.length - 1; i >= 0; i--) {
-            paintGroup[i].setStrokeWidth(radius/3);
-                canvas.drawPoint(xyGroup[i][0], xyGroup[i][1], paintGroup[i]);
+            canvas.drawPoint(xyGroup[i][0], xyGroup[i][1], paintGroup[i]);
         }
 
+        //计算下一次点的位置
         for (int i = 0; i < paintGroup.length; i++) {
-            if (zeroGroup[i]){
+            if (zeroGroup[i]) {
                 if (i == 0 || angleGroup[i - 1] >= 15) {
                     xyGroup[i][0] = x - (int) (radius * Math.cos(Math.toRadians(angleGroup[i])));
                     xyGroup[i][1] = y - (int) (radius * Math.sin(Math.toRadians(angleGroup[i])));
@@ -91,21 +133,22 @@ public class LoadingView extends View {
                         offsetGroup[i] += ACELERATION;
                     else
                         offsetGroup[i] -= ACELERATION;
-                    if (angleGroup[i] >= 359&&angleGroup[angleGroup.length-1]<=359) {
-                        angleGroup[i] =360;
-                        offsetGroup[i] =360;
-                        zeroGroup[i]=false;
+                    if (angleGroup[i] >= 359 && angleGroup[angleGroup.length - 1] <= 359) {
+                        angleGroup[i] = 360;
+                        offsetGroup[i] = 360;
+                        zeroGroup[i] = false;
                     }
-                    if(angleGroup[i] >= 359&&angleGroup[angleGroup.length-1]>359){
-                        angleGroup =new double[]{0,0,0,0,0,0};
-                        offsetGroup =new double[]{OFFSET,OFFSET,OFFSET,OFFSET,OFFSET,OFFSET};
-                        zeroGroup =new boolean[]{true, true, true, true, true, true};
+                    if (angleGroup[i] >= 359 && angleGroup[angleGroup.length - 1] > 359) {
+                        angleGroup = new double[]{0, 0, 0, 0, 0, 0};
+                        offsetGroup = new double[]{OFFSET, OFFSET, OFFSET, OFFSET, OFFSET, OFFSET};
+                        zeroGroup = new boolean[]{true, true, true, true, true, true};
                     }
                 }
             }
 
         }
 
+        //在延迟时间后重画
         postInvalidateDelayed(REFRASHTIME);
 
     }
